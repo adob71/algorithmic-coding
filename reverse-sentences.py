d0c2a95f9d6482ef408f2e303e77f8bfe9728bfa
adob71 <adob71@gmail.com>
2022-06-09 05:53:30 +0200

    commit

--- reverse-sentences.py
+++ reverse-sentences.py
@@ -1,9 +1,20 @@
+#usage
+#python3 reverse-sentences.py
+#input
+#reverse-sentences.input
+#output
+#reverse-sentences.output
+
+#driver code
+
 filei = open("reverse-sentences.input","r")
-fileo = open("reverse-sentences.output","w")
 n=int(filei.readline())
+
+fileo = open("reverse-sentences.output","w")
+
 for i in range(1,n+1):
   linei=filei.readline()
-  linei = linei.rstrip()
+  linei=linei.rstrip()
   wordi=linei.split(" ")
 #  print(wordi)
   l=len(wordi)
